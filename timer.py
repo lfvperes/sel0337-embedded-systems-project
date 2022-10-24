680da70fdca3f0244c83a4db34026fa1c1c4345c
lfvperes <luisfvperes@gmail.com>
2022-10-24 11:43:25 -0300

    implementing PWM to timer

--- timer.py
+++ timer.py
@@ -6,7 +6,7 @@
 # LUIS FILIPE VASCONCELOS PERES 10310641
 
 from time import sleep
-from gpiozero import LED
+from gpiozero import LED, PWMLED
 
 def contagem_regressiva(time_seconds):
     while time_seconds > 0:
@@ -32,9 +32,18 @@ while not input_success:
         input_success = False
 
 contagem_regressiva(time_seconds)
-my_led = LED(18)
+my_led = PWMLED(18)
 while True:
-    my_led.on()
+    # my_led.on()
+    # sleep(0.5)
+    # my_led.off()
+    # sleep(0.5)
+
+    # implementing PWM
+    my_led.value = 0
     sleep(0.5)
-    my_led.off()
+    my_led.value = 0.5
     sleep(0.5)
+    my_led.value = 1
+    sleep(0.5)
+
