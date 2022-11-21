7acb4ff91d4c9e21f599d7b92219c1c6c9a45496
Luís Peres <luisfvperes@gmail.com>
2022-11-21 12:14:42 -0300

    adding multithreading code

--- pratica5/multithread.py
+++ pratica5/multithread.py
@@ -5,7 +5,49 @@
 # CAMILA HIROMI TANAKA 10748201
 # LUIS FILIPE VASCONCELOS PERES 10310641
 
-import threading
-import RPi.GPIO
+import threading as th
+import RPi.GPIO as GPIO
 from time import sleep
 
+#freq = 1/4
+period = 4
+blink = True
+
+led = 18
+button = 15
+
+def button_callback(channel):
+    global period
+    if GPIO.input(button):
+        print("Interrupt")
+        period = 2
+    else: period = 4
+
+def timer_callback():
+    global blink
+    print("fim do timer")
+    blink = False    
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(
+    button,
+    GPIO.IN,
+    pull_up_down=GPIO.PUD_UP
+)
+GPIO.add_event_detect(button,
+    GPIO.BOTH,
+    callback=button_callback,
+    bouncetime=50
+)
+GPIO.setup(led, GPIO.OUT)
+
+S = th.Timer(35, timer_callback)
+S.start()
+
+while blink:
+    GPIO.output(led, GPIO.HIGH)
+    sleep(period)
+    GPIO.output(led, GPIO.LOW)
+    sleep(period)
+    print(period)
+GPIO.cleanup()
