f7581378eaf080ac403f2f44a91980d88cfd1d95
lfvperes <luisfvperes@gmail.com>
2022-12-05 11:31:22 -0300

    adding activity 6 folder

---
+++ pratica6/cv_api.py
@@ -0,0 +1,12 @@
+# UNIVERSIDADE DE SAO PAULO 2022
+# SEL0337 - APLICACAO DE MICROPROCESSADORES II
+# PRATICA 6
+# 
+# CAMILA HIROMI TANAKA 10748201
+# LUIS FILIPE VASCONCELOS PERES 10310641
+# github.com/lfvperes/sel0337-embedded-systems-project
+
+import PiCamera, requests, json, pprint
+
+station_ID = 966583
+
