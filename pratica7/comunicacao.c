9fe55242b4cd476e688041d3693e5c598dc863ae
CamilaHTanaka <53956319+CamilaHTanaka@users.noreply.github.com>
2022-12-19 11:14:18 -0300

    Update comunicacao.c

--- pratica7/comunicacao.c
+++ pratica7/comunicacao.c
@@ -7,9 +7,12 @@ CAMILA HIROMI TANAKA 10748201
 LUIS FILIPE VASCONCELOS PERES 10310641
 */
 
+# Definindo pinagem analogica do arduino
 int pot = A0;
 int data = A4;
 int clk = A5;
+
+
 
 void setup() {
     pinMode(ledPin)
